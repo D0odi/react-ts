541dfe27245754bd4bc4a7403bf560eadd6d9bb9
unknown <lnatekin@asu.edu>
2024-04-16 17:11:17 -0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+type User = {
+  username: string;
+  id: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState<User | null>(null);
 
   return (
     <>
-      <div class="">
-        <header class="m-auto">
-          <p
-            class="text-2xl underline underline-offset-8 font-['Menlo'] 
-          hover:underline-offset-4 hover:text-3xl hover:border-red-500 
-           hover:border-4 hover:p-4 hover:cursor-pointer"
-          >
-            wagwann
-          </p>
-        </header>
-      </div>
+      <div className=""></div>
     </>
   );
 }
